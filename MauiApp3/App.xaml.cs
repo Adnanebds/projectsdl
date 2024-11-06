89b82369cc11eca113786b0d6a4bd6c373d7dd69
Adnanebds <adnanebadysoussi47@gmail.com>
2024-11-06 16:33:43 +0100

    login and home page

---
+++ MauiApp3/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiApp3.Components;
+
+namespace MauiApp3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+           
+            MainPage = new NavigationPage(new HomePage());
+
+        }
+    }
+}
---
+++ MauiApp3/Components/HomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp3.Components
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for navigating to LoginPage
+        private async void NavigateToLoginPage(object sender, EventArgs e)
+        {
+            // Navigate to LoginPage
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
